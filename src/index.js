5125c8f1181223c24de6e00094efd31fda1875c3
Junaid Zafar <33980511+junaidmzafar@users.noreply.github.com>
2018-10-08 17:03:00 +0100

    Added imports to main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './css/styles.css';
 import './css/layout.css';
+
 import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
